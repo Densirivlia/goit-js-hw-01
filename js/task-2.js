d65e607915ed781b5634ac56c22eb1a6c5e6675d
DenSiri <denis.sirivlya@gmail.com>
2025-10-24 18:56:58 +0300

    task 1-2-3.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,5 @@
 function getShippingMessage(country, price, deliveryFee) {
-  const totalCost = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalCost} credits`;
+  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
 
 console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
